7e7ed6b2ab61e50523aba10b9af5082b9cd8a8ef
Zack <zackary12389@yahoo.co.uk>
2021-12-12 20:08:58 +0000

    Add index.ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+import Bolton from './lib/Bolton';
+import CambridgeCity from './lib/CambridgeCity';
+import HuntingdonDistrict from './lib/HuntingdonDistrict';
+import PeterboroughCity from './lib/PeterboroughCity';
+
+export default {
+    Bolton,
+    CambridgeCity,
+    HuntingdonDistrict,
+    PeterboroughCity
+};
